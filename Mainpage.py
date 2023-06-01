268a3c7b30ceb754faedcfe2ee4eaedf947c3223
norsyahidahzul <100520992+norsyahidahzul@users.noreply.github.com>
2023-06-01 14:31:14 +0800

    Create Mainpage.py

---
+++ Mainpage.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Interactive Dashboard_COVID-19 in Malaysia during Pre-Vaccination Period",
+    page_icon="🦠")
+	
+st.write("# Welcome to Interactive Dashboard of COVID-19 in Malaysia during Pre-Vaccination Period! 👋")
+
+
+st.markdown(
+    """
+    This interactive dashboard of COVID-19 in Malaysia is made for research purpose of Master's degree. This dashboard
+	contains COVID-19 simulator and COVID-19 predictor. This dashboard is mainly
+	builded for researcher, analyst and government agency to help making a proper decision upon the spike or subside of COVID-19 cases.
+	
+    **👈 Click the pages on the sidebar** to explore this interactive dashboard!
+    
+"""
+)
